14de7613bfed212a26772b6ec828fa9e4c26a151
GTT\jriera <jriera@gtt.es>
2024-03-13 13:06:33 +0100

    prueba

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
+
+
+Vue.filter('toMoneda', function (value) {
+  if (typeof value !== "number") {
+      value = parseFloat(value)
+  }
+  var formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+  });
+  
+  return formatter.format(value);
+  
+});
